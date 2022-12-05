84400d339bdd9d62847ef64c0f6c73ad287e9993
Jean-Luc TAN <jeanluc.tan75@gmail.com>
2022-12-05 10:31:22 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import cookChef from "../assets/img/cookchef.png" ;
+
+function Header() {
+  return (
+    <header className="header d-flex flex-row align-items-center">
+        <i class="fa-solid fa-bars mr-15"/>
+        <div className="logo flex-fill">
+            <img src={cookChef} alt="logo" />
+        </div>
+        <ul>
+            <button className="mr-5 btn btn-reverse-primary">
+             <i class="fa-solid fa-basket-shopping mr-5"></i>
+              <span>Panier</span>
+            </button>
+            <button className="btn btn-primary">Connexion</button>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
